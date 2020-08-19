a38e06759c412dd2f8d6ec62568b1113e0fdf08b
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-08-19 18:44:58 -0300

    Updates on PixelDrawer

--- src/main/java/com/nickmafra/gfx/PixelDrawer.java
+++ src/main/java/com/nickmafra/gfx/PixelDrawer.java
@@ -7,19 +7,25 @@ public class PixelDrawer {
 
     private final int width;
     private final int height;
+    private final int scale;
     private final BiFunction<Integer, Integer, Color> pixelColor;
 
-    public PixelDrawer(int width, int height, BiFunction<Integer, Integer, Color> pixelColor) {
+    public PixelDrawer(int width, int height, int scale, BiFunction<Integer, Integer, Color> pixelColor) {
         this.width = width;
         this.height = height;
+        this.scale = scale;
         this.pixelColor = pixelColor;
     }
 
+    public PixelDrawer(int width, int height, BiFunction<Integer, Integer, Color> pixelColor) {
+        this(width, height, 1, pixelColor);
+    }
+
     public void draw(Graphics2D g) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y ++) {
+            for (int x = 0; x < width; x ++) {
                 g.setColor(pixelColor.apply(x, y));
-                g.drawRect(x, y, 1, 1);
+                g.fillRect(x * scale, y * scale, scale, scale);
             }
         }
     }
